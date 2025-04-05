7cd73c36578eba6189c31a8107a0c1c8f08dbc5b
Balu-Palle <pallebalu18@gmail.com>
2025-04-05 17:12:43 +0530

    third commit

--- linked-list/middle.java
+++ linked-list/middle.java
@@ -9,6 +9,13 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     }
 public class middle {
     public ListNode middleNode(ListNode head) {
         if(head.next==null || head==null) return head;
