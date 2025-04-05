79cd5252f947a56e1a939ab5f3f9750c3411b086
Balu-Palle <pallebalu18@gmail.com>
2025-04-05 17:09:22 +0530

    second commit

--- linked-list/mergeSorted.java
+++ linked-list/mergeSorted.java
@@ -1,3 +1,10 @@
+public class ListNode {
+         int val;
+         ListNode next;
+         ListNode() {}
+         ListNode(int val) { this.val = val; }
+         ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     }
 public class mergeSorted {
     /**
  * Definition for singly-linked list.
