e7b709bf41a23d2a1a23553e0b2b43ed2dac7d3d
Balu-Palle <pallebalu18@gmail.com>
2025-04-05 17:00:47 +0530

    initial commit

---
+++ linked-list/middle.java
@@ -0,0 +1,24 @@
+/**
+ ****************LEETCODE*********
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+public class middle {
+    public ListNode middleNode(ListNode head) {
+        if(head.next==null || head==null) return head;
+        ListNode s=head,f=head;
+        while(f.next!=null && f.next.next!=null){
+            s=s.next;
+            f=f.next.next;
+        }
+        if (f.next==null) return s;
+        return s.next;
+        
+    }
+}
